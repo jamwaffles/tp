809b76e7631c0096eaeb0ee43ce49f1cd98a7f41
James Waples <james@wapl.es>
2023-10-17 11:56:00 +0100

    Partial fix for dual deceleration periods

--- src/trapezoidal_non_zero.rs
+++ src/trapezoidal_non_zero.rs
@@ -112,6 +112,10 @@ impl Segment {
         let t_a = (vlim - v0) / a_max;
         let t_d = (vlim - v1) / a_max;
 
+        // If we need to decelerate twice instead of accelerating then decelerating, this ensures
+        // the first decel time is always positive. Negative time makes no sense.
+        let t_a = t_a.abs();
+
         // Total duration of this segment
         let total_time = if v_lim_reached {
             h / v_max
